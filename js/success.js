e52eea03f7cf5dba1829c1e158a282edb0404099
Saif Ali <imshaiksaif@gmail.com>
2022-01-19 18:56:21 +0530

    user redirection fix with strapi

---
+++ js/success.js
@@ -0,0 +1,46 @@
+const STAPIURL = `https://jumpgram-strapi.herokuapp.com/api/insta-users`;
+
+class HandleUserRedirection {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.getUserTokenFromStrapi();
+  }
+
+  async getUserTokenFromStrapi() {
+    console.log("getUserTokenFromStrapi");
+    const username = this.getUserNameFromURL();
+    if (username) {
+      const response = await fetch(
+        `${STAPIURL}?filters[username][$eq]=${username}&sort[0]=publishedAt%3Adesc&pagination[limit]=1`
+      );
+      const resData = await response.json();
+
+      if (resData && resData.data && resData.data.length > 0) {
+        const user = resData.data[0];
+        this.redirectToUserProfile(user);
+      } else {
+        document.querySelector(
+          "[page-title='true']"
+        ).innerHTML = `No User Found with username ${username}`;
+        document.querySelector(".loader").style.display = "none";
+      }
+    }
+  }
+
+  getUserNameFromURL() {
+    const url = new URL(window.location.href);
+    const username = url.searchParams.get("username");
+    return username || null;
+  }
+
+  redirectToUserProfile(user) {
+    const { token } = user?.attributes || {};
+    const url = `http://app.jumpgram.co/#${token}`;
+    window.location.href = url;
+  }
+}
+
+new HandleUserRedirection();
