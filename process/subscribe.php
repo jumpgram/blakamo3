6b70861a3db7c107d74a643096ed7f018d20dc60
jonathancabato <60134987+jonathancabato@users.noreply.github.com>
2022-04-12 19:29:20 +0800

    Create subscription for chargebee API

---
+++ process/subscribe.php
@@ -0,0 +1,38 @@
+<?php 
+
+header('Content-Type: application/json');
+
+
+function curl_post($url, $post = null){
+    $ch = curl_init();
+    $post = http_build_query($post);
+
+    curl_setopt($ch, CURLOPT_URL, $url );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_USERPWD, 'live_tingsUBvTvCOg4eqI5dEpc2kUFGsE8z2' . ':' . '');
+
+    $headers = array();
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+
+    return $result;
+}
+$customer_id = $_POST['customer_id'];
+
+$s_resp = curl_post('https://jump-gram.chargebee.com/api/v2/customers/' . $customer_id . '/subscription_for_items', array(
+    'subscription_items' => array(
+        'item_price_id' => array('Paid-plan-USD-Monthly'),
+        'quantity' => array(1)
+    )
+    ));
+
+
+echo json_encode(json_decode($s_resp));
