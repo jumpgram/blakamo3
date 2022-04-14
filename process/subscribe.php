4cb1d03b14278cc3d4ac416ecc3813460a1e821c
jonathancabato <60134987+jonathancabato@users.noreply.github.com>
2022-04-14 19:08:32 +0800

    Added coupon to the Chargebee API

--- process/subscribe.php
+++ process/subscribe.php
@@ -11,7 +11,7 @@ function curl_post($url, $post = null){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_USERPWD, 'live_tingsUBvTvCOg4eqI5dEpc2kUFGsE8z2' . ':' . '');
+    curl_setopt($ch, CURLOPT_USERPWD, 'live_tingsUBvTvCOg4eqI5dEpc2kUFGsE8z2' . ':' . ''); // LIVE
 
     $headers = array();
     $headers[] = 'Content-Type: application/x-www-form-urlencoded';
@@ -26,13 +26,22 @@ function curl_post($url, $post = null){
     return $result;
 }
 $customer_id = $_POST['customer_id'];
-
-$s_resp = curl_post('https://jump-gram.chargebee.com/api/v2/customers/' . $customer_id . '/subscription_for_items', array(
-    'subscription_items' => array(
-        'item_price_id' => array('Paid-plan-USD-Monthly'),
-        'quantity' => array(1)
-    )
-    ));
-
+$coupon_id = isset($_POST['coupon_id']) ? $_POST['coupon_id'] : '';
+if(!empty($coupon_id)){
+    $s_resp = curl_post('https://jump-gram.chargebee.com/api/v2/customers/' . $customer_id . '/subscription_for_items', array(
+        'coupon_ids' => array($coupon_id),
+        'subscription_items' => array(
+            'item_price_id' => array('Paid-plan-USD-Monthly'), //LIVE
+            'quantity' => array(1)
+        )
+        ));
+}else{
+    $s_resp = curl_post('https://jump-gram.chargebee.com/api/v2/customers/' . $customer_id . '/subscription_for_items', array(
+        'subscription_items' => array(
+            'item_price_id' => array('Paid-plan-USD-Monthly'), //LIVE
+            'quantity' => array(1)
+        )
+        ));
+}
 
 echo json_encode(json_decode($s_resp));
